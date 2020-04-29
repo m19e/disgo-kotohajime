74cd41604fd6a736a39ed872a429933dac10671a
m19e <nexus.me7290@gmail.com>
2020-04-30 01:42:32 +0900

    Add pingpong reaction

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module disgo
 
 go 1.14
 
-require github.com/joho/godotenv v1.3.0
+require (
+	github.com/bwmarrin/discordgo v0.20.3 // indirect
+	github.com/joho/godotenv v1.3.0
+)
--- main.go
+++ main.go
@@ -4,7 +4,10 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"os/signal"
+	"syscall"
 
+	"github.com/bwmarrin/discordgo"
 	"github.com/joho/godotenv"
 )
 
@@ -24,7 +27,47 @@ func init() {
 }
 
 func main() {
-	fmt.Println("Hello, DisGo.")
-	fmt.Printf("Token: %s\n", Token)
-	fmt.Printf("BotName: %s\n", BotName)
+	// Create a new Discord session.
+	dg, err := discordgo.New("Bot " + Token); if err != nil {
+		fmt.Println("Error creating Discord session: ", err)
+		return
+	}
+
+	// Register the messageCreate func as a callback
+	dg.AddHandler(messageCreate)
+
+	// Open a websocket connection to Discord and begin listening.
+	err = dg.Open(); if err != nil {
+		fmt.Println("Error opening connection: ", err)
+		return
+	}
+
+	// Wait here until Ctrl-C or other term signal is received.
+	fmt.Println("Bot is now running. Press Ctrl-C to exit.")
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+	<-sc
+
+	// Cleanly close down the Discord session.
+	dg.Close()
+}
+
+
+// This func will be called every time a new message
+// is created on any channel that bot has access to.
+func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
+	// Ignore all messages created by the bot itself.
+	if m.Author.ID == s.State.User.ID {
+		return
+	}
+
+	// If the message is "ping" reply with "Pong!".
+	if m.Content == "ping" {
+		s.ChannelMessageSend(m.ChannelID, "Pong!")
+	}
+
+	// If the message is "pong" reply with "Ping!".
+	if m.Content == "pong" {
+		s.ChannelMessageSend(m.ChannelID, "Ping!")
+	}
 }
